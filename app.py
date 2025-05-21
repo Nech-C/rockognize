100677702350ce9329b017f01fbb06854d5c1cd1
Nech-C <xiaokunchen@outlook.com>
2025-05-20 22:07:44 -0400

    feat: improve layout and add examples

--- app.py
+++ app.py
@@ -19,15 +19,38 @@ def classify_image(image):
     for item in result:
         output[item['label']] = item['score']
     return output
-demo = gr.Interface(
-    fn=classify_image,
-    inputs=gr.Image(type="pil"),
-    outputs=gr.Label(num_top_classes=5),
-    title="Rockognize",
-    description="A simple app to classify rocks and minerals.",
-    theme="default",
-)
 
+examples = [
+    ["examples/quartz.jpg"],
+    ["examples/agate.jpg"],
+    ["examples/topaz.jpg"],
+]
+with gr.Blocks() as demo:
+    gr.Markdown("# 🪨 Rockognize")
+    gr.Markdown("Upload an image of a rock or mineral and get its top predictions.")
 
+    with gr.Tab("Mineral Image Classification"):
+        with gr.Row(height="80%"):
+            with gr.Column():
+                image_input = gr.Image(type="pil", label="upload Image", height=300)
+                submit_button = gr.Button("Classify")
+                gr.Examples(
+                    examples=examples,
+                    inputs=image_input,
+                    outputs=label_output,
+                    label="Try Examples",
+                    examples_per_page=3,
+                    cache_examples=False,
+                    fn=classify_image,
+                )
+            with gr.Column():
+                label_output = gr.Label(label="Top 5 Predictions")
+        
+
+            submit_button.click(
+                classify_image,
+                inputs=image_input,
+                outputs=label_output
+            )
 
 demo.launch()
